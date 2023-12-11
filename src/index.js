a7125566643bfbc22f1ba81b9efcf170aa893637
Arath Campos <mi.familia.55555@gmail.com>
2023-12-11 13:06:00 -0600

    Added all dependecies nedded

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// build: 
+//      npm run build
+//      npx webpack b
+import "@material/web/button/filled-button"
+import "@material/web/button/outlined-button"
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// build: npx webpack --config webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'src'),
+  },
+  mode: 'production'
+};
